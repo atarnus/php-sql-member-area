1c5037b4d7ffa8ea4b2176468f769a8b1ac8b0e4
Katariina Pohto <63994411+atarnus@users.noreply.github.com>
2023-02-06 12:14:53 +0200

    Small fixes to load CSS
    * Firefox FOUC fix
    * CSS force reload

--- php/head.php
+++ php/head.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html">
 <html">
 	<head>
+		<script>
+			/*to prevent Firefox FOUC, this must be here*/
+			let FF_FOUC_FIX;
+		</script>
 		<meta charset="UTF-8">
 		<title><?php echo($title); ?></title>
-		<link href="css/css.css" rel="stylesheet" type="text/css">
+		<link href="css/css.css?rnd=12345" rel="stylesheet" type="text/css">
 	</head>
